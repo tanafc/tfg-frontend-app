681d05fd3d68aabfd316f4b374fc10ed4e1379bb
Tanausu <tanausu@acidtango.com>
2023-04-19 23:55:03 +0100

    Feat: Add SignupScreen

---
+++ src/screens/SignupScreen/SignupScreen.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
+import CustomButton, {
+  CustomButtonTypes,
+} from "../../components/CustomButton/CustomButton";
+import CustomInput from "../../components/CustomInput/CustomInput";
+import { SignupScreenRouteProps } from "../../models/Navigation";
+
+const SignupScreen = ({ navigation }: SignupScreenRouteProps) => {
+  const [username, setUsername] = React.useState<string>("");
+  const [email, setEmail] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+  const [passwordRepeat, setPasswordRepeat] = React.useState<string>("");
+
+  const handleSignup = () => {
+    Alert.alert("Username: " + username + " Password: " + password);
+  };
+
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ flexGrow: 1 }}
+    >
+      <View style={styles.container}>
+        <View style={styles.titleView}>
+          <Text style={styles.titleText}>Signup</Text>
+        </View>
+
+        <View style={styles.inputView}>
+          <Text style={styles.labelInput}>Username</Text>
+          <CustomInput
+            placeholder="John Doe"
+            value={username}
+            setValue={setUsername}
+          />
+
+          <Text style={styles.labelInput}>Email</Text>
+          <CustomInput
+            placeholder="example@email.com"
+            value={email}
+            setValue={setEmail}
+          />
+
+          <Text style={styles.labelInput}>Password</Text>
+          <CustomInput
+            placeholder="*********"
+            value={password}
+            setValue={setPassword}
+            secureTextEntry
+          />
+
+          <Text style={styles.labelInput}>Confirm Password</Text>
+          <CustomInput
+            placeholder="*********"
+            value={passwordRepeat}
+            setValue={setPasswordRepeat}
+            secureTextEntry
+          />
+
+          <CustomButton text="Signup" onPress={handleSignup} />
+
+          <CustomButton
+            text="Go to Login"
+            onPress={() => navigation.navigate("LoginScreen")}
+            type={CustomButtonTypes.TERTIARY}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#DFDDC7",
+  },
+
+  inputView: {
+    width: "70%",
+  },
+
+  labelInput: {
+    marginTop: 5,
+  },
+
+  titleView: {
+    marginBottom: 10,
+  },
+
+  titleText: {
+    fontWeight: "bold",
+    fontStyle: "italic",
+    fontSize: 30,
+  },
+});
+
+export default SignupScreen;
