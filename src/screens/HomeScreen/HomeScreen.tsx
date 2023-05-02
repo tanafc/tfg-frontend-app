1b88ec44323a43ca9e397d4844194edfcebb2f32
Tanausu <tanausu@acidtango.com>
2023-05-02 19:51:49 +0100

    Feat: Remove logout from HomeScreen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,19 +1,10 @@
-import { StackActions } from "@react-navigation/native";
 import React from "react";
-import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
-import CustomButton from "../../components/CustomButton";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { useAuth } from "../../context/AuthContext";
-import { useLogout } from "../../hooks/useLogout";
-import { HomeScreenRouteProps } from "../../models/Navigation";
+import { HomeScreenNavigationProps } from "../../models/Navigation";
 
-const HomeScreen = ({ navigation }: HomeScreenRouteProps) => {
+const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
   const { account } = useAuth();
-  const logout = useLogout();
-
-  const handleLogout = async () => {
-    await logout();
-    navigation.dispatch(StackActions.popToTop());
-  };
 
   return (
     <ScrollView
@@ -23,17 +14,6 @@ const HomeScreen = ({ navigation }: HomeScreenRouteProps) => {
       <View style={styles.container}>
         <View style={styles.titleView}>
           <Text style={styles.titleText}>Welcome back, {account.username}</Text>
-        </View>
-
-        <View style={styles.buttonView}>
-          <CustomButton
-            text="Have a nice day"
-            onPress={() => Alert.alert("I love you <3")}
-          />
-        </View>
-
-        <View style={styles.buttonView}>
-          <CustomButton text="Logout" onPress={handleLogout} />
         </View>
       </View>
     </ScrollView>
