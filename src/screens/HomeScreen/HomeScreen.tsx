1adbd3b23ae041ec35886deac47baed38486b475
Tanausu <tanausu@acidtango.com>
2023-06-06 21:32:27 +0100

    Feat: List products seen in home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,14 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import React from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { useAuth } from "../../context/AuthContext";
+import { useProductContext } from "../../context/ProductContext";
 import { HomeScreenNavigationProps } from "../../models/Navigation";
 
 const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
   const { account } = useAuth();
+
+  const { products } = useProductContext();
 
   return (
     <ScrollView
@@ -13,8 +17,31 @@ const HomeScreen = ({ navigation }: HomeScreenNavigationProps) => {
     >
       <View style={styles.container}>
         <View style={styles.titleView}>
-          <Text style={styles.titleText}>Welcome back, {account.username}</Text>
+          <Text style={styles.titleText}>Welcome {account.username}</Text>
         </View>
+
+        {products.length > 0 && (
+          <View style={styles.productContainer}>
+            <Text style={styles.subtitleText}>Last products seen 👀</Text>
+            <View style={styles.productList}>
+              {[...products].reverse().map((product) => (
+                <Pressable
+                  key={product.barcode}
+                  onPress={() =>
+                    navigation.navigate("Product", {
+                      screen: "ProductScreen",
+                      params: { barcode: product.barcode },
+                    })
+                  }
+                  style={styles.productRow}
+                >
+                  <Ionicons name="arrow-redo" size={25} color="tomato" />
+                  <Text style={styles.productText}>{product.name}</Text>
+                </Pressable>
+              ))}
+            </View>
+          </View>
+        )}
       </View>
     </ScrollView>
   );
@@ -28,10 +55,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#DFDDC7",
   },
 
-  buttonView: {
-    width: "80%",
-  },
-
   titleView: {
     marginBottom: 15,
   },
@@ -40,6 +63,28 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontStyle: "italic",
     fontSize: 30,
+  },
+
+  subtitleText: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+
+  productContainer: {
+    width: "80%",
+  },
+
+  productList: {},
+
+  productRow: {
+    marginTop: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+
+  productText: {
+    fontSize: 16,
   },
 });
 
